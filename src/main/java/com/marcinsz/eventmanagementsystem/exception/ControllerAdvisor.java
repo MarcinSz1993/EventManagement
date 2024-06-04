de89dfbb2dbdb680c03a7b793a815c01e3aa2cda
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-06-04 14:53:35 +0200

    Handle MethodArgumentNotValidException in ControllerAdvisor class.

--- src/main/java/com/marcinsz/eventmanagementsystem/exception/ControllerAdvisor.java
+++ src/main/java/com/marcinsz/eventmanagementsystem/exception/ControllerAdvisor.java
@@ -1,10 +1,17 @@
 package com.marcinsz.eventmanagementsystem.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.function.Consumer;
 
 @ControllerAdvice
 public class ControllerAdvisor {
@@ -32,5 +39,21 @@ public class ControllerAdvisor {
     @ExceptionHandler(EventException.class)
     public String EventExceptionHandler(EventException ex){
         return ex.getMessage();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex){
+        HashMap<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(new Consumer<ObjectError>() {
+                    @Override
+                    public void accept(ObjectError error) {
+                        String field = ((FieldError) error).getField();
+                        String message = error.getDefaultMessage();
+                        errors.put(field,message);
+                    }
+                });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 }
