6197a87860f1ce66921ac8860168bb96f29942aa
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-02-14 10:28:55 +0100

    Small modifications.

--- src/main/java/com/marcinsz/eventmanagementsystem/configuration/SecurityConfig.java
+++ src/main/java/com/marcinsz/eventmanagementsystem/configuration/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("https://eventmanagementwebapplication.netlify.app"));
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
